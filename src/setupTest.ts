e953936ea6a6143eebdc8278b4bf77436ba69c13
Esteban Beltran <esteban.beltran@tcit.cl>
2022-12-05 19:25:59 -0300

    feat: config project with test and msw

---
+++ src/setupTest.ts
@@ -0,0 +1,8 @@
+import 'whatwg-fetch'
+
+import { server } from './mocks/server'
+import { beforeAll, afterAll, afterEach } from 'vitest';
+
+beforeAll(() => server.listen({ onUnhandledRequest: 'error' }))
+afterAll(() => server.close())
+afterEach(() => server.resetHandlers())
