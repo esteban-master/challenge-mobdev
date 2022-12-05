cc3f5c975f2d73a23cea84f9e182ce2d2dba519a
Esteban Beltran <esteban.beltran@tcit.cl>
2022-12-05 19:34:33 -0300

    feat: add material ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,24 @@
-import reactLogo from './assets/react.svg'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 import './App.css'
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { Button } from '@mui/material';
+
+const queryClient = new QueryClient()
 
 function App() {
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <div className="App">
+        <h1>Vite + React</h1>
+        <Button variant="contained">Hello World</Button>
       </div>
-      <h1>Vite + React</h1>
-    </div>
+    </QueryClientProvider>
   )
 }
 
